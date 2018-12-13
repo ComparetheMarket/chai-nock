ae7a77eb88396e7ff419df091e2c01411beefb3f
Rick Clark <rkingsleyclark@gmail.com>
2018-12-13 07:59:29 +0000

    Make linting changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-"use strict";
+/* eslint-disable no-underscore-dangle */
 
 const equal = require('deep-equal');
 
-module.exports = (chai) => {
-  const Assertion = chai.Assertion;
+module.exports = chai => {
+  const { Assertion } = chai;
   const MAX_TIMEOUT = 2000;
 
   function promisfyNockInterceptor(nock) {
@@ -36,36 +36,56 @@ module.exports = (chai) => {
 
   function isNock(obj) {
     if (
-      typeof(obj) !== 'object' ||
-      !obj.interceptors || 
-      !obj.interceptors.length) 
-    {
+      typeof obj !== 'object' ||
+      !obj.interceptors ||
+      !obj.interceptors.length
+    ) {
       throw new TypeError('You must provide a valid Nock');
-    }    
+    }
   }
 
-  Assertion.addProperty('requested', function () {
+  Assertion.addProperty('requested', () => {
     isNock(this._obj);
-    const assert = (value) => {
-      this.assert(value, 'expected Nock to have been requested', 'expected Nock to have not been requested');
-    }
-
-    return promisfyNockInterceptor(this._obj)
-      .then(
-        () => assert(true), 
-        () => assert(false)
+    const assert = value => {
+      this.assert(
+        value,
+        'expected Nock to have been requested',
+        'expected Nock to have not been requested',
       );
+    };
+
+    return promisfyNockInterceptor(this._obj).then(
+      () => assert(true),
+      () => assert(false),
+    );
   });
 
-  Assertion.addMethod('requestedWith', function (arg) {
+  Assertion.addMethod('requestedWith', arg => {
     isNock(this._obj);
 
-    return promisfyNockInterceptor(this._obj)
-      .then((nockRequest) => {
+    return promisfyNockInterceptor(this._obj).then(
+      nockRequest => {
         if (equal(nockRequest, arg)) {
-          return this.assert(true, null, 'expected Nock to have not been requested with #{exp}', arg);
+          return this.assert(
+            true,
+            null,
+            'expected Nock to have not been requested with #{exp}',
+            arg,
+          );
         }
-        return this.assert(false, 'expected Nock to have been requested with #{exp}, but was requested with #{act}', 'expected Nock to have not been requested with #{exp}', arg, nockRequest);
-      },() => this.assert(false, 'expected Nock to have been requested, but it was never called'));
+        return this.assert(
+          false,
+          'expected Nock to have been requested with #{exp}, but was requested with #{act}',
+          'expected Nock to have not been requested with #{exp}',
+          arg,
+          nockRequest,
+        );
+      },
+      () =>
+        this.assert(
+          false,
+          'expected Nock to have been requested, but it was never called',
+        ),
+    );
   });
 };
