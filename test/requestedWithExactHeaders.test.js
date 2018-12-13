951dd431d574978faa4080c54ef26985865c4c03
Rick Clark <rkingsleyclark@gmail.com>
2018-12-13 08:47:53 +0000

    Lint new test files

--- test/requestedWithExactHeaders.test.js
+++ test/requestedWithExactHeaders.test.js
@@ -1,100 +1,100 @@
-const { expect, use } = require("chai");
-const nock = require("nock");
-const request = require("request-promise-native");
+const { expect, use } = require('chai');
+const nock = require('nock');
+const request = require('request-promise-native');
 
-const chaiNock = require("..");
+const chaiNock = require('..');
+
 use(chaiNock);
 
-describe("requestedWithExactHeaders() assertions", () => {
-  const TEST_URL = "http://someurl.com";
+describe('requestedWithExactHeaders() assertions', () => {
+  const TEST_URL = 'http://someurl.com';
 
   const requestObj = {
     json: true,
     uri: TEST_URL,
     headers: {
-      test: 123
-    }
+      test: 123,
+    },
   };
 
   afterEach(() => {
     nock.cleanAll();
   });
 
-  describe("when asserting on a type that is not a Nock", () => {
-    it("throws a type error", () => {
+  describe('when asserting on a type that is not a Nock', () => {
+    it('throws a type error', () => {
       expect(() =>
-        expect("NOT_A_NOCK").to.have.been.requestedWithExactHeaders()
+        expect('NOT_A_NOCK').to.have.been.requestedWithExactHeaders(),
       ).to.throw(TypeError);
 
       expect(() =>
-        expect({}).to.have.been.requestedWithExactHeaders()
+        expect({}).to.have.been.requestedWithExactHeaders(),
       ).to.throw(TypeError);
 
       expect(() =>
         expect(
-          nock("http://url-without.a").get("/interceptor")
-        ).to.have.been.requestedWithExactHeaders()
+          nock('http://url-without.a').get('/interceptor'),
+        ).to.have.been.requestedWithExactHeaders(),
       ).to.throw(TypeError);
     });
   });
-  
-  describe(".requestedWithExactHeaders()", () => {
 
-    describe("when a request to the nock has been made with the correct argument", () => {
-      describe("with an Object as an argument", () => {
-        it("passes", () => {
+  describe('.requestedWithExactHeaders()', () => {
+    describe('when a request to the nock has been made with the correct argument', () => {
+      describe('with an Object as an argument', () => {
+        it('passes', () => {
           const requestNock = nock(TEST_URL)
-            .get("/")
+            .get('/')
             .reply(200);
           request(requestObj);
 
           return expect(requestNock).to.have.been.requestedWithExactHeaders({
             test: 123,
-            host: "someurl.com",
-            accept: "application/json"
+            host: 'someurl.com',
+            accept: 'application/json',
           });
         });
       });
     });
 
-    describe("when a request to the nock has been made but with incorrect arguments", () => {
-      it("throws", done => {
+    describe('when a request to the nock has been made but with incorrect arguments', () => {
+      it('throws', done => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
         request(requestObj);
 
         const assertion = expect(
-          requestNock
+          requestNock,
         ).to.have.been.requestedWithExactHeaders({ test: 2 });
-        const actualHeaders = "{ Object (test, host, ...) }"; // Chai truncates the object to this string
+        const actualHeaders = '{ Object (test, host, ...) }'; // Chai truncates the object to this string
 
         return assertion
-          .then(() => done.fail("Should have thrown an error"))
+          .then(() => done.fail('Should have thrown an error'))
           .catch(err => {
             expect(err.message).to.contain(
-              `expected Nock to have been requested with exact headers { test: 2 }, but was requested with headers ${actualHeaders}`
+              `expected Nock to have been requested with exact headers { test: 2 }, but was requested with headers ${actualHeaders}`,
             );
             done();
           });
       });
     });
 
-    describe("when a request to the nock has not been made", () => {
-      it("throws", done => {
+    describe('when a request to the nock has not been made', () => {
+      it('throws', done => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
 
         const assertion = expect(
-          requestNock
+          requestNock,
         ).to.have.been.requestedWithExactHeaders({ test: 123 });
 
         return assertion
-          .then(() => done.fail("Should have thrown an error"))
+          .then(() => done.fail('Should have thrown an error'))
           .catch(err => {
             expect(err.message).to.equal(
-              "expected Nock to have been requested, but it was never called"
+              'expected Nock to have been requested, but it was never called',
             );
             done();
           });
@@ -102,53 +102,53 @@ describe("requestedWithExactHeaders() assertions", () => {
     });
   });
 
-  describe(".not.requestedWithExactHeaders()", () => {
-    describe("when a request to the nock has been made with the incorrect arguments", () => {
-      it("passes", () => {
+  describe('.not.requestedWithExactHeaders()', () => {
+    describe('when a request to the nock has been made with the incorrect arguments', () => {
+      it('passes', () => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
         request(requestObj);
 
         return expect(requestNock).not.to.have.been.requestedWithExactHeaders(
-          "different_value"
+          'different_value',
         );
       });
     });
 
-    describe("when a request to the nock has not been made", () => {
-      it("passes", () => {
+    describe('when a request to the nock has not been made', () => {
+      it('passes', () => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
 
         return expect(requestNock).not.to.have.been.requestedWithExactHeaders(
-          "different_value"
+          'different_value',
         );
       });
     });
 
-    describe("when a request to the nock has been made with matching arguments", () => {
-      it("throws", done => {
+    describe('when a request to the nock has been made with matching arguments', () => {
+      it('throws', done => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
         request(requestObj);
 
         const assertion = expect(
-          requestNock
+          requestNock,
         ).not.to.have.been.requestedWithExactHeaders({
           test: 123,
-          host: "someurl.com",
-          accept: "application/json"
+          host: 'someurl.com',
+          accept: 'application/json',
         });
-        const headersString = "{ Object (test, host, ...) }"; // Chai truncates the object to this string
+        const headersString = '{ Object (test, host, ...) }'; // Chai truncates the object to this string
 
         return assertion
-          .then(() => done.fail("Should have thrown an error"))
+          .then(() => done.fail('Should have thrown an error'))
           .catch(err => {
             expect(err.message).to.equal(
-              `expected Nock to have not been requested with exact headers ${headersString}`
+              `expected Nock to have not been requested with exact headers ${headersString}`,
             );
             done();
           });
--- test/requestedWithHeaders.test.js
+++ test/requestedWithHeaders.test.js
@@ -1,12 +1,13 @@
-const { expect, use } = require("chai");
-const nock = require("nock");
-const request = require("request-promise-native");
+const { expect, use } = require('chai');
+const nock = require('nock');
+const request = require('request-promise-native');
 
-const chaiNock = require("..");
+const chaiNock = require('..');
+
 use(chaiNock);
 
-describe("requestedWithHeaders() assertions", () => {
-  const TEST_URL = "http://someurl.com";
+describe('requestedWithHeaders() assertions', () => {
+  const TEST_URL = 'http://someurl.com';
 
   const requestObj = {
     json: true,
@@ -14,131 +15,130 @@ describe("requestedWithHeaders() assertions", () => {
     headers: {
       test: 123,
       test2: 456,
-    }
+    },
   };
 
   afterEach(() => {
     nock.cleanAll();
   });
 
-  describe("when asserting on a type that is not a Nock", () => {
-    it("throws a type error", () => {
+  describe('when asserting on a type that is not a Nock', () => {
+    it('throws a type error', () => {
       expect(() =>
-        expect("NOT_A_NOCK").to.have.been.requestedWithHeaders()
+        expect('NOT_A_NOCK').to.have.been.requestedWithHeaders(),
       ).to.throw(TypeError);
 
-      expect(() =>
-        expect({}).to.have.been.requestedWithHeaders()
-      ).to.throw(TypeError);
+      expect(() => expect({}).to.have.been.requestedWithHeaders()).to.throw(
+        TypeError,
+      );
 
       expect(() =>
         expect(
-          nock("http://url-without.a").get("/interceptor")
-        ).to.have.been.requestedWithHeaders()
+          nock('http://url-without.a').get('/interceptor'),
+        ).to.have.been.requestedWithHeaders(),
       ).to.throw(TypeError);
     });
   });
-  
-  describe(".requestedWithHeaders()", () => {
 
-    describe("when a request to the nock has been made with the correct argument", () => {
-        describe("when the argument object fuzzy matches the request headers", () => {
-          it("passes", () => {
-            const requestNock = nock(TEST_URL)
-              .get("/")
-              .reply(200);
-            request(requestObj);
-  
-            return expect(requestNock).to.have.been.requestedWithHeaders({
-              test: 123
-            });
+  describe('.requestedWithHeaders()', () => {
+    describe('when a request to the nock has been made with the correct argument', () => {
+      describe('when the argument object fuzzy matches the request headers', () => {
+        it('passes', () => {
+          const requestNock = nock(TEST_URL)
+            .get('/')
+            .reply(200);
+          request(requestObj);
+
+          return expect(requestNock).to.have.been.requestedWithHeaders({
+            test: 123,
           });
         });
+      });
 
-        describe("when the argument object exactly matches the request headers", () => {
-          it("passes", () => {
-            const requestNock = nock(TEST_URL)
-              .get("/")
-              .reply(200);
-            request(requestObj);
-  
-            return expect(requestNock).to.have.been.requestedWithHeaders({
-              test: 123,
-              test2: 456,
-              host: "someurl.com",
-              accept: "application/json"
-            });
+      describe('when the argument object exactly matches the request headers', () => {
+        it('passes', () => {
+          const requestNock = nock(TEST_URL)
+            .get('/')
+            .reply(200);
+          request(requestObj);
+
+          return expect(requestNock).to.have.been.requestedWithHeaders({
+            test: 123,
+            test2: 456,
+            host: 'someurl.com',
+            accept: 'application/json',
           });
         });
+      });
     });
 
-    describe("when a request to the nock has been made but with incorrect arguments", () => {
-      describe("with an Object as an argument", () => {
-        describe("when none of the key/value pairs in the object are in the request headers", () => {
-          it("throws", done => {
+    describe('when a request to the nock has been made but with incorrect arguments', () => {
+      describe('with an Object as an argument', () => {
+        describe('when none of the key/value pairs in the object are in the request headers', () => {
+          it('throws', done => {
             const requestNock = nock(TEST_URL)
-              .get("/")
+              .get('/')
               .reply(200);
             request(requestObj);
-            
+
             const assertion = expect(
-              requestNock
+              requestNock,
             ).to.have.been.requestedWithHeaders({ wrongKey: 789 });
 
-            const actualHeaders = "{ Object (test, test2, ...) }"; // Chai truncates the object to this string
-         
+            const actualHeaders = '{ Object (test, test2, ...) }'; // Chai truncates the object to this string
+
             return assertion
-              .then(() => done.fail("Should have thrown an error"))
+              .then(() => done.fail('Should have thrown an error'))
               .catch(err => {
                 expect(err.message).to.contain(
-                  `expected Nock to have been requested with headers { wrongKey: 789 }, but was requested with headers ${actualHeaders}`
+                  `expected Nock to have been requested with headers { wrongKey: 789 }, but was requested with headers ${actualHeaders}`,
                 );
                 done();
               });
           });
         });
 
-        describe("when only one of the key/value pairs in the object is in the request headers", () => {
-          it("throws", done => {
+        describe('when only one of the key/value pairs in the object is in the request headers', () => {
+          it('throws', done => {
             const requestNock = nock(TEST_URL)
-              .get("/")
+              .get('/')
               .reply(200);
             request(requestObj);
-    
+
             const assertion = expect(
-              requestNock
+              requestNock,
             ).to.have.been.requestedWithHeaders({ wrongKey: 789, test: 123 });
-            
-            const actualHeaders = "{ Object (test, test2, ...) }"; // Chai truncates the object to this string
-    
+
+            const actualHeaders = '{ Object (test, test2, ...) }'; // Chai truncates the object to this string
+
             return assertion
-              .then(() => done.fail("Should have thrown an error"))
+              .then(() => done.fail('Should have thrown an error'))
               .catch(err => {
                 expect(err.message).to.contain(
-                  `expected Nock to have been requested with headers { wrongKey: 789, test: 123 }, but was requested with headers ${actualHeaders}`
+                  `expected Nock to have been requested with headers { wrongKey: 789, test: 123 }, but was requested with headers ${actualHeaders}`,
                 );
                 done();
               });
           });
 
-          describe("when the object and headers share a key but with different values", () => {
-            it("throws", done => {
+          describe('when the object and headers share a key but with different values', () => {
+            it('throws', done => {
               const requestNock = nock(TEST_URL)
-                .get("/")
+                .get('/')
                 .reply(200);
               request(requestObj);
-      
+
               const assertion = expect(
-                requestNock
+                requestNock,
               ).to.have.been.requestedWithHeaders({ test: 'DifferentValue' });
-              
-              const actualHeaders = "{ Object (test, test2, ...) }"; // Chai truncates the object to this string
-      
+
+              const actualHeaders = '{ Object (test, test2, ...) }'; // Chai truncates the object to this string
+
               return assertion
-                .then(() => done.fail("Should have thrown an error"))
+                .then(() => done.fail('Should have thrown an error'))
                 .catch(err => {
                   expect(err.message).to.contain(
-                    `expected Nock to have been requested with headers { test: 'DifferentValue' }, but was requested with headers ${actualHeaders}`
+                    `expected Nock to have been requested with headers { test: 'DifferentValue' }, but was requested with headers ${actualHeaders}`,
                   );
                   done();
                 });
@@ -148,21 +148,21 @@ describe("requestedWithHeaders() assertions", () => {
       });
     });
 
-    describe("when a request to the nock has not been made", () => {
-      it("throws", done => {
+    describe('when a request to the nock has not been made', () => {
+      it('throws', done => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
 
-        const assertion = expect(
-          requestNock
-        ).to.have.been.requestedWithHeaders({ test: 123 });
+        const assertion = expect(requestNock).to.have.been.requestedWithHeaders(
+          { test: 123 },
+        );
 
         return assertion
-          .then(() => done.fail("Should have thrown an error"))
+          .then(() => done.fail('Should have thrown an error'))
           .catch(err => {
             expect(err.message).to.equal(
-              "expected Nock to have been requested, but it was never called"
+              'expected Nock to have been requested, but it was never called',
             );
             done();
           });
@@ -170,50 +170,50 @@ describe("requestedWithHeaders() assertions", () => {
     });
   });
 
-  describe(".not.requestedWithHeaders()", () => {
-    describe("when a request to the nock has been made with the incorrect arguments", () => {
-      it("passes", () => {
+  describe('.not.requestedWithHeaders()', () => {
+    describe('when a request to the nock has been made with the incorrect arguments', () => {
+      it('passes', () => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
         request(requestObj);
 
         return expect(requestNock).not.to.have.been.requestedWithHeaders(
-          "different_value"
+          'different_value',
         );
       });
     });
 
-    describe("when a request to the nock has not been made", () => {
-      it("passes", () => {
+    describe('when a request to the nock has not been made', () => {
+      it('passes', () => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
 
         return expect(requestNock).not.to.have.been.requestedWithHeaders(
-          "different_value"
+          'different_value',
         );
       });
     });
 
-    describe("when a request to the nock has been made with matching arguments", () => {
-      it("throws", done => {
+    describe('when a request to the nock has been made with matching arguments', () => {
+      it('throws', done => {
         const requestNock = nock(TEST_URL)
-          .get("/")
+          .get('/')
           .reply(200);
         request(requestObj);
 
         const assertion = expect(
-          requestNock
+          requestNock,
         ).not.to.have.been.requestedWithHeaders({
-          test: 123
+          test: 123,
         });
 
         return assertion
-          .then(() => done.fail("Should have thrown an error"))
+          .then(() => done.fail('Should have thrown an error'))
           .catch(err => {
             expect(err.message).to.equal(
-              'expected Nock to have not been requested with headers { test: 123 }'
+              'expected Nock to have not been requested with headers { test: 123 }',
             );
             done();
           });
