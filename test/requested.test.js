1ef2e54b64cf2a13ebcccd32ca4592c6343e32dc
Rick Clark <rkingsleyclark@gmail.com>
2018-12-13 09:16:10 +0000

    Linting (#6)
    * Install linting deps
    
    * Set linting config
    
    * Make linting changes

--- test/requested.test.js
+++ test/requested.test.js
@@ -1,8 +1,9 @@
-const { expect, use} = require('chai');
+const { expect, use } = require('chai');
 const nock = require('nock');
 const request = require('request-promise-native');
 
 const chaiNock = require('../');
+
 use(chaiNock);
 
 describe('requested assertions', () => {
@@ -14,16 +15,16 @@ describe('requested assertions', () => {
 
   describe('when asserting on a type that is not a Nock', () => {
     it('throws a type error', () => {
-      expect(
-        () => expect('NOT_A_NOCK').to.have.been.requested
-      ).to.throw(TypeError);
+      expect(() => expect('NOT_A_NOCK').to.have.been.requested).to.throw(
+        TypeError,
+      );
+
+      expect(() => expect({}).to.have.been.requested).to.throw(TypeError);
 
       expect(
-        () => expect({}).to.have.been.requested
-      ).to.throw(TypeError);
-
-      expect(
-        () => expect(nock('http://url-without.a').get('/interceptor')).to.have.been.requested
+        () =>
+          expect(nock('http://url-without.a').get('/interceptor')).to.have.been
+            .requested,
       ).to.throw(TypeError);
     });
   });
@@ -31,7 +32,9 @@ describe('requested assertions', () => {
   describe('.requested', () => {
     describe('when a request to the nock has been made', () => {
       it('passes', () => {
-        const requestNock = nock(TEST_URL).get('/').reply(200);
+        const requestNock = nock(TEST_URL)
+          .get('/')
+          .reply(200);
         request(TEST_URL);
 
         return expect(requestNock).to.have.been.requested;
@@ -39,32 +42,40 @@ describe('requested assertions', () => {
     });
 
     describe('when a request to the nock has not been made', () => {
-      it('throws', (done) => {
-        const requestNock = nock(TEST_URL).get('/').reply(200);
+      it('throws', done => {
+        const requestNock = nock(TEST_URL)
+          .get('/')
+          .reply(200);
 
         const assertion = expect(requestNock).to.have.been.requested;
 
         return assertion
           .then(() => done.fail('Should have thrown an error'))
-          .catch((err) => {
-            expect(err.message).to.equal('expected Nock to have been requested');
+          .catch(err => {
+            expect(err.message).to.equal(
+              'expected Nock to have been requested',
+            );
             done();
           });
       });
     });
 
     describe('when a there is an error in the nock', () => {
-      it('throws', (done) => {
-        const requestNock = nock(TEST_URL).get('/').reply(200);
+      it('throws', done => {
+        const requestNock = nock(TEST_URL)
+          .get('/')
+          .reply(200);
 
         const assertion = expect(requestNock).to.have.been.requested;
 
-        requestNock.emit('error', new Error('A problem with Nock'))
+        requestNock.emit('error', new Error('A problem with Nock'));
 
         return assertion
           .then(() => done.fail('Should have thrown an error'))
-          .catch((err) => {
-            expect(err.message).to.equal('expected Nock to have been requested');
+          .catch(err => {
+            expect(err.message).to.equal(
+              'expected Nock to have been requested',
+            );
             done();
           });
       });
@@ -74,23 +85,29 @@ describe('requested assertions', () => {
   describe('.not.requested', () => {
     describe('when a request to the nock has not been made', () => {
       it('passes', () => {
-        const requestNock = nock(TEST_URL).get('/').reply(200);
+        const requestNock = nock(TEST_URL)
+          .get('/')
+          .reply(200);
 
         return expect(requestNock).not.to.have.been.requested;
       });
     });
 
     describe('when a request to the nock has been made', () => {
-      it('throws', (done) => {
-        const requestNock = nock(TEST_URL).get('/').reply(200);
+      it('throws', done => {
+        const requestNock = nock(TEST_URL)
+          .get('/')
+          .reply(200);
         request(TEST_URL);
 
         const assertion = expect(requestNock).not.to.have.been.requested;
 
         return assertion
           .then(() => done.fail('Should have thrown an error'))
-          .catch((err) => {
-            expect(err.message).to.equal('expected Nock to have not been requested');
+          .catch(err => {
+            expect(err.message).to.equal(
+              'expected Nock to have not been requested',
+            );
             done();
           });
       });
